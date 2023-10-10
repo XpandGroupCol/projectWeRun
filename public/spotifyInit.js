00e1590409a5b428b9cc951395207c55c6a10bf3
Lady9696 <leidysuarez55@gmail.com>
2023-10-10 16:12:35 -0500

    spotify funciona

--- public/spotifyInit.js
+++
@@ -1,9 +0,0 @@
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    const element = document.getElementById('embed-iframe');
-    const options = {
-        uri: 'spotify:episode:6q9xgGXdlSNTMM1zrXOxXw'
-      };
-    const callback = (EmbedController) => {};
-    IFrameAPI.createController(element, options, callback);
-  };
-  
