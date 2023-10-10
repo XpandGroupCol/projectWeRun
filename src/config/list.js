e74cb9655e649208bd1873905d4fab1e090d7fd4
Lady9696 <leidysuarez55@gmail.com>
2023-10-10 17:01:50 -0500

    landing finished

--- src/config/list.js
+++ src/config/list.js
@@ -19,25 +19,26 @@ export const ICONS = [
   {
     name: "tiktok",
     src: "tiktok.svg",
-    
+
     link: "https://www.tiktok.com/@proyectowerun",
   },
   {
     name: "spotify",
     src: "spotify.svg",
-   
+
     link: "https://open.spotify.com/show/0yXt7UtjjX9GyccFwfaVLp?si=64a932bf85d14016&nd=1",
   },
 
   {
     name: "youtube",
     src: "youtube.svg",
-  
+
     link: "https://www.youtube.com/@ProyectoWeRun",
   },
 ];
 export const LINKSTORE = [
   {
+    id: "bogota-store",
     name: "Nike Store Bogotá",
     src: "wpplogo.svg",
     src2: "wppblack.svg",
@@ -45,30 +46,35 @@ export const LINKSTORE = [
   },
 
   {
+    id: "medellin-store",
     name: "Nike Store Medellín",
     src: "wpplogo.svg",
     src2: "wppblack.svg",
     link: "https://api.whatsapp.com/send?phone=573208576389&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
   },
   {
+    id: "pereira-store",
     name: "Nike Store Pereira",
     src: "wpplogo.svg",
     src2: "wppblack.svg",
     link: "https://api.whatsapp.com/send?phone=573134786372&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
   },
   {
+    id: "panama-store",
     name: "Nike Store Panamá",
     src: "wpplogo.svg",
     src2: "wppblack.svg",
     link: "https://www.sportline.com.pa/marcas/nike.html",
   },
   {
+    id: "guatemala-store",
     name: "Nike Store Guatemala",
     src: "wpplogo.svg",
-      src2: "wppblack.svg",
+    src2: "wppblack.svg",
     link: "https://api.whatsapp.com/send?phone=573134786372&text=%C2%A1Estoy%20interesado%20en%20productos%20NIKE!",
   },
   {
+    id: "costarica-store",
     name: "Nike Store Costa Rica",
     src: "wpplogo.svg",
     src2: "wppblack.svg",
